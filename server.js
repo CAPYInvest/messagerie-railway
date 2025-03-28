86186b244dcbcc54067114c94719c6487348b50a
CAPYInvest <capy.invest@gmail.com>
2025-03-28 22:38:38 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 const connectedClients = {};
-const fetch = require('node-fetch');
 
 const { initializeApp } = require('firebase/app');
 const {
