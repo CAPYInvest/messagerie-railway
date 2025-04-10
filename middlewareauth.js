478b40a5de6a238102f90ecc9cde998eff823e96
CAPYInvest <capy.invest@gmail.com>
2025-04-10 16:48:57 +0200

    Add files via upload

--- middlewareauth.js
+++ middlewareauth.js
@@ -3,11 +3,13 @@ const jwt = require('jsonwebtoken');
 
 async function requireAuth(req, res, next) {
   const authHeader = req.headers.authorization;
+  console.log("Header Authorization reçu:", authHeader);  // Pour débogage
+
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ error: 'Authentification requise' });
   }
   const token = authHeader.split(' ')[1];
-  console.log("Token reçu :", token); // Vérifiez ici la valeur du token
+  console.log("Token extrait :", token); // Vérifiez ici la valeur du token
 
   try {
     // Vérifier que le token est signé avec la bonne clé
