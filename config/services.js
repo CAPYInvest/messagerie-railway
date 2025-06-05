50d780f06b9401b6f7eb8df3bc3ca8def90b6104
CAPYInvest <capy.invest@gmail.com>
2025-06-05 17:21:21 +0200

    Add files via upload

--- config/services.js
+++ config/services.js
@@ -1,1 +1,23 @@
+/**
+ * Configuration des services externes
+ * @module config/services
+ */
 
+// Configuration Google Calendar
+const googleConfig = {
+  clientId: process.env.GOOGLE_CLIENT_ID_CALENDAR,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET_CALENDAR,
+  redirectUri: process.env.GOOGLE_REDIRECT_URI_CALENDAR,
+  scopes: ['https://www.googleapis.com/auth/calendar']
+};
+
+// Configuration Brevo (Sendinblue)
+const brevoConfig = {
+  apiKey: process.env.BREVO_API_KEY_CALENDAR,
+  senderEmail: process.env.BREVO_SENDER_EMAIL_CALENDAR
+};
+
+module.exports = {
+  googleConfig,
+  brevoConfig
+};
