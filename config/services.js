b7897dab5cba8dd0111e9d6b93ae36b6ba51faaf
CAPYInvest <capy.invest@gmail.com>
2025-06-08 15:33:47 +0200

    Add files via upload

--- config/services.js
+++ config/services.js
@@ -3,13 +3,26 @@
  * @module config/services
  */
 
-// Configuration Google Calendar
+require('dotenv').config();
+
+// Configuration Google
 const googleConfig = {
-  clientId: process.env.GOOGLE_CLIENT_ID_CALENDAR,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET_CALENDAR,
-  redirectUri: process.env.GOOGLE_REDIRECT_URI_CALENDAR,
-  scopes: ['https://www.googleapis.com/auth/calendar']
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  redirectUri: process.env.GOOGLE_REDIRECT_URI || 'https://capy-invest-fr.webflow.io/compte-utilisateur',
+  scopes: [
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.events'
+  ]
 };
+
+// Vérification de la configuration
+console.log('[Config] Vérification de la configuration Google:', {
+  clientId: googleConfig.clientId ? 'Défini' : 'Non défini',
+  clientSecret: googleConfig.clientSecret ? 'Défini' : 'Non défini',
+  redirectUri: googleConfig.redirectUri,
+  scopes: googleConfig.scopes
+});
 
 // Configuration Brevo (Sendinblue)
 const brevoConfig = {
